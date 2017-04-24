bb08564e893330541939a7a5d9eac74ce2083b39
niuwanpeng <niuwanpeng@baidu.com>
2017-04-24 17:19:43 +0800

    test compile

--- src/main/java/com/jlu/release/Application.java
+++ src/main/java/com/jlu/release/Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created by niuwanpeng on 17/4/6.
@@ -22,5 +26,16 @@ public class Application implements EmbeddedServletContainerCustomizer {
     @Override
     public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
         configurableEmbeddedServletContainer.setPort(8998);
+    }
+
+    @Bean
+    public MultipartConfigElement createMultipartConfigElement()
+    {
+        MultipartConfigFactory mcf = new MultipartConfigFactory();
+        /**
+         * 设置最大上传文件的大小，默认是1000MB
+         */
+        mcf.setMaxFileSize("1000MB");
+        return mcf.createMultipartConfig();
     }
 }
